50e234e63e95d4a6da906325c6001b33da73976d
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-06-03 21:10:22 +0200

    Update s.lua

--- teleport/s.lua
+++ teleport/s.lua
@@ -1,1 +1,31 @@
-a.lia
+function findPlayerByName(name)
+    local players = getElementsByType("player")
+    for _, player in ipairs(players) do
+        if getPlayerName(player) == name then
+            return player
+        end
+    end
+    return false
+end
+
+function teleportPlayerToPlayer(player, targetPlayerName)
+    if not targetPlayerName then
+        outputChatBox("Błąd: nie wskazano gracza.", player, 255, 0, 0)
+        return
+    end
+
+    local targetPlayer = findPlayerByName(targetPlayerName)
+    if not targetPlayer then
+        outputChatBox("Gracz o nicku " .. targetPlayerName .. " nie został znaleziony.", player, 255, 0, 0)
+        return
+    end
+
+    local x, y, z = getElementPosition(targetPlayer)
+    setElementPosition(player, x, y, z)
+    outputChatBox("Zostałeś przeteleportowany do " .. getPlayerName(targetPlayer) .. ".", player, 0, 255, 0)
+end
+
+addEvent("onPlayerRequestTeleport", true)
+addEventHandler("onPlayerRequestTeleport", root, function(targetPlayerName)
+    teleportPlayerToPlayer(client, targetPlayerName)
+end)
